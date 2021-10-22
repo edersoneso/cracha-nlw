a9d3d8edbf0f178719bbdc2a9454bb704a11100f
Ederson Souza <63760035+edersoneso@users.noreply.github.com>
2021-10-22 10:52:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'edersoneso',
+  youtube: 'UC5vFT1qiNjrGL8_SRiJcXXQ',
+  facebook: 'luciane.diniz.378',
+  instagram: 'ederson.eso',
+  twitter: 'eso_Ederson'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
